016d3c95217e8f6818822939c5cb24a44bcacea3
snerall1 <snerall1@asu.edu>
2022-10-27 20:24:07 -0700

    first junit commit

---
+++ src/main/java/org/example/urinals.java
@@ -0,0 +1,28 @@
+package org.example;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class urinals {
+    public static void main(String[] args) throws Exception {
+
+
+    }
+
+    public static void countUrinals(String str) {
+        System.out.println("Not yet implemented");
+
+    }
+
+    public static void readFile() {
+        System.out.println("Not yet implemented");
+
+    }
+    public static void writeFile() {
+        System.out.println("Not yet implemented");
+
+    }
+}
---
+++ src/test/java/org/example/urinalsTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+public class urinalsTest {
+
+@Test
+void countUrinalsTest()
+ {
+  Assertions.assertEquals(false, true);
+  System.out.println("====== Sai Sunil Neralla == TEST ONE EXECUTED =======");
+ }
+
+}
